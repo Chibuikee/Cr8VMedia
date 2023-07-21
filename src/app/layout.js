bad703cfa0b403cb2b4adcbd569456cdebd95202
Chibuike Ewenike <sopewenike@yahoo.com>
2023-07-21 23:26:06 +0100

    favicon updated for multiple platforms

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,14 +10,36 @@ const hind = Hind({
 });
 
 export const metadata = {
-  title: "BUZO PORTFOLIO",
-  description: "Portfolio of Chibuzo a Media Creative with a decade experience",
+  title: "CR8VIBE MEDIA",
+  description:
+    "Official website of CR8VIBE MEDIA, a media company with a decade experience",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${hind.variable} font-sans`}>
       <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
