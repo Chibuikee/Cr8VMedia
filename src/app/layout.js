ca955b79affd56b582835f44c6348e01aa71224f
Chibuike Ewenike <sopewenike@yahoo.com>
2023-07-22 00:08:22 +0100

    favicon bug corrected

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${hind.variable} font-sans`}>
       <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
